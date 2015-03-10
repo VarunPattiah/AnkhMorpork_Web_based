d275d1a1c45bcbbc039143738dc96522142fe698
taiseemdadwal <taiseem.dadwal@gmail.com>
2015-03-10 12:19:23 -0400

    Test suite case added to web version of project
    test suite case

---
+++ src/com/concordia/ankhMorPork/manager/AllTests.java
@@ -0,0 +1,12 @@
+package com.concordia.ankhMorPork.manager;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AreaTest.class, BoardManagerTest.class, BoardTest.class,
+		PlayerTest.class })
+public class AllTests {
+
+}
